4ab05aa3e0564030b97a874564b10864df17ca76
Rashed Bishal <rashed.bishal@live.com>
2025-03-13 11:57:33 +0600

    add aliiance information configuration file

---
+++ config/alliance.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'brand_name' => 'Alliance Builders Ltd.',
+    'brand_name_absolute' => 'Alliance Builders Limited',
+    'email_address' => 'asad@alliancebuildersltd.org',
+    'phone_number' => '+88-01896204440',
+    'url' => 'https://www.alliancebuildersltd.org',
+    'social_links' => [
+        'facebook' => 'https://www.facebook.com/alliancebuilderslimited',
+        'youtube' => 'https://www.youtube.com/@shahid549',
+    ],
+    'google_map' => [
+        'embed' => 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3650.5645659550537!2d90.36879267602399!3d23.798515286926186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c12fae71412b%3A0xb6df06aab789d2d1!2sAlliance%20Builders%20Ltd!5e0!3m2!1sen!2sbd!4v1741845234378!5m2!1sen!2sbd',
+        'share' => 'https://maps.app.goo.gl/EVMXw1drQZah1iK7A',
+    ],
+];
